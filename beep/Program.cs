09de8f2f3d39df36e13092c86dedd6712f671aeb
Diego De Oliveira Freitas <35883602800@info.local>
2019-10-11 07:50:57 -0300

    mess

---
+++ beep/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace beep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Beep(1728, 3000);
+            Console.Beep(1728, 2000);
+            Console.Beep(1728, 1000);
+            Console.Beep(1728, 3000);
+            Console.Beep(1728, 4000);
+            Console.Beep(1728, 500);
+            Console.Beep(1728, 1500);
+            Console.Beep(1728, 3000);
+        }
+    }
+}
